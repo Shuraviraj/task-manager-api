c2b79649c9df44743ebe7e823da9613600f129af
ravi raj <ravi.official097@gmail.com>
2026-05-03 15:59:40 +0530

    rename

--- src/main/java/com/taskmanager/task_api/repository/UserRepository.java
+++ src/main/java/com/taskmanager/task_api/repository/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
-    @Query("select u from User u where u.username = ?1")
+    @Query("select a from AppUser a where a.username = ?1")
     Optional<AppUser> findByUsername(String username);
 }
