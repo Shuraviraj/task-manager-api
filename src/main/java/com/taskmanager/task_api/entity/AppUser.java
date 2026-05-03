48a0fafcd59d86bde6e547feade45a212e7e9b16
ravi raj <ravi.official097@gmail.com>
2026-05-03 15:31:55 +0530

    renamed User to AppUser

--- src/main/java/com/taskmanager/task_api/entity/AppUser.java
+++ src/main/java/com/taskmanager/task_api/entity/AppUser.java
@@ -10,7 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
--- src/main/java/com/taskmanager/task_api/repository/UserRepository.java
+++ src/main/java/com/taskmanager/task_api/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.taskmanager.task_api.repository;
 
-import com.taskmanager.task_api.entity.User;
+import com.taskmanager.task_api.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<AppUser, Long> {
     @Query("select u from User u where u.username = ?1")
-    Optional<User> findByUsername(String username);
+    Optional<AppUser> findByUsername(String username);
 }
